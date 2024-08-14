b82c73277aad12061b4ac15596ddcd3e7c9f57c5
Vladimir Vrzić <vvrzic@gmail.com>
2024-08-14 18:14:49 +0200

    Indented code with GNU indent
    Options:
    
    --no-tabs -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb
    -ce -ci4 -cli4 -d0 -di1 -nfc1 -i4 -ip0 -l80 -lp -npcs -nprs
    -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1

--- src/inphdl/inphdl.c
+++ src/inphdl/inphdl.c
@@ -31,9 +31,7 @@ struct IHandler {
 
 struct IHandler IHandler;
 
-
 void gfxWaitTOF(void);
-
 
 static void inpDoPseudoMultiTasking(void)
 {
@@ -74,14 +72,14 @@ void inpOpenAllInputDevs(void)
 void inpCloseAllInputDevs(void)
 {
     /*
-    if (SDL_JoystickOpened(0)) {
-        SDL_JoystickClose(IHandler.Joystick);
-    }
+       if (SDL_JoystickOpened(0)) {
+       SDL_JoystickClose(IHandler.Joystick);
+       }
 
-    if (SDL_WasInit(SDL_INIT_JOYSTICK) != 0) {
-        SDL_QuitSubSystem(SDL_INIT_JOYSTICK);
-    }
-    */
+       if (SDL_WasInit(SDL_INIT_JOYSTICK) != 0) {
+       SDL_QuitSubSystem(SDL_INIT_JOYSTICK);
+       }
+     */
 }
 
 void inpMousePtrOn(void)
@@ -101,11 +99,11 @@ S32 inpWaitFor(S32 l_Mask)
     U32 WaitTime = 0;
 
     if ((IHandler.EscStatus) && (!(l_Mask & INP_NO_ESC))) {
-	l_Mask |= INP_ESC;
+        l_Mask |= INP_ESC;
     }
 
     if ((IHandler.FunctionKeyStatus) && (!(l_Mask & INP_FUNCTION_KEY))) {
-	l_Mask |= INP_FUNCTION_KEY;
+        l_Mask |= INP_FUNCTION_KEY;
     }
 
     action = 0;
@@ -114,154 +112,155 @@ S32 inpWaitFor(S32 l_Mask)
     WaitTime = 0;
 
     while (action == 0) {
-	gfxWaitTOF();
+        gfxWaitTOF();
 
-	WaitTime++;
-	/* Abfrage des Zeit-Flags */
-	if ((l_Mask & INP_TIME) && WaitTime >= IHandler.ul_WaitTicks) {
-	    action |= INP_TIME;
+        WaitTime++;
+        /* Abfrage des Zeit-Flags */
+        if ((l_Mask & INP_TIME) && WaitTime >= IHandler.ul_WaitTicks) {
+            action |= INP_TIME;
         }
 
-	while (SDL_PollEvent(&ev)) {
-	    switch (ev.type) {
-	    case SDL_WINDOWEVENT:
-		{
-		    switch (ev.window.event) {
-			    case SDL_WINDOWEVENT_SIZE_CHANGED:
-			    case SDL_WINDOWEVENT_EXPOSED:
-				    if (setup.WindowKeepAspect) gfxResizeToFit();
-				    gfxRefreshArea(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-			    break;
-			    default:
-			    break;
-		    }
-		}
-		break;
-	    case SDL_QUIT:
-		tcQuit();
-		break;
-	    case SDL_KEYDOWN:
-		{
-		    switch (ev.key.keysym.sym) {
-		    case SDLK_f:
-			if (setup.FullScreen) {
-			    SDL_SetWindowFullscreen(getMainSDLWindow(), 0);
-			    setup.FullScreen = false;
-			} else {
-			    SDL_SetWindowFullscreen(getMainSDLWindow(), GFX_SDL_FULLSCREEN);
-			    setup.FullScreen = true;
-			}
-			break;
-		    case SDLK_k:
-			setup.WindowKeepAspect = ! setup.WindowKeepAspect;
-			if (setup.WindowKeepAspect) gfxResizeToFit();
-			break;
-		    case SDLK_LEFT:
-			if ((l_Mask & INP_LEFT))
-			    action |= INP_KEYBOARD + INP_LEFT;
-			break;
+        while (SDL_PollEvent(&ev)) {
+            switch (ev.type) {
+                case SDL_WINDOWEVENT:
+                    switch (ev.window.event) {
+                        case SDL_WINDOWEVENT_SIZE_CHANGED:
+                        case SDL_WINDOWEVENT_EXPOSED:
+                            if (setup.WindowKeepAspect)
+                                gfxResizeToFit();
+                            gfxRefreshArea(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case SDL_QUIT:
+                    tcQuit();
+                    break;
+                case SDL_KEYDOWN:
+                    switch (ev.key.keysym.sym) {
+                        case SDLK_f:
+                            if (setup.FullScreen) {
+                                SDL_SetWindowFullscreen(getMainSDLWindow(), 0);
+                                setup.FullScreen = false;
+                            } else {
+                                SDL_SetWindowFullscreen
+                                    (getMainSDLWindow(), GFX_SDL_FULLSCREEN);
+                                setup.FullScreen = true;
+                            }
+                            break;
+                        case SDLK_k:
+                            setup.WindowKeepAspect = !setup.WindowKeepAspect;
+                            if (setup.WindowKeepAspect)
+                                gfxResizeToFit();
+                            break;
+                        case SDLK_LEFT:
+                            if ((l_Mask & INP_LEFT))
+                                action |= INP_KEYBOARD + INP_LEFT;
+                            break;
 
-		    case SDLK_RIGHT:
-			if ((l_Mask & INP_RIGHT))
-			    action |= INP_KEYBOARD + INP_RIGHT;
-			break;
+                        case SDLK_RIGHT:
+                            if ((l_Mask & INP_RIGHT))
+                                action |= INP_KEYBOARD + INP_RIGHT;
+                            break;
 
-		    case SDLK_UP:
-			if ((l_Mask & INP_UP))
-			    action |= INP_KEYBOARD + INP_UP;
-			break;
+                        case SDLK_UP:
+                            if ((l_Mask & INP_UP))
+                                action |= INP_KEYBOARD + INP_UP;
+                            break;
 
-		    case SDLK_DOWN:
-			if ((l_Mask & INP_DOWN))
-			    action |= INP_KEYBOARD + INP_DOWN;
-			break;
+                        case SDLK_DOWN:
+                            if ((l_Mask & INP_DOWN))
+                                action |= INP_KEYBOARD + INP_DOWN;
+                            break;
 
-		    case SDLK_SPACE:
-		    case SDLK_RETURN:
-		    case SDLK_KP_ENTER:
-			if ((l_Mask & (INP_LBUTTONP | INP_LBUTTONR)))
-			    action |= INP_KEYBOARD + INP_LBUTTONP;
-			break;
+                        case SDLK_SPACE:
+                        case SDLK_RETURN:
+                        case SDLK_KP_ENTER:
+                            if ((l_Mask & (INP_LBUTTONP | INP_LBUTTONR)))
+                                action |= INP_KEYBOARD + INP_LBUTTONP;
+                            break;
 
-		    case SDLK_ESCAPE:
-			if (IHandler.EscStatus)
-			    action |= INP_KEYBOARD + INP_ESC;
-			break;
+                        case SDLK_ESCAPE:
+                            if (IHandler.EscStatus)
+                                action |= INP_KEYBOARD + INP_ESC;
+                            break;
 
-		    case SDLK_F1:
-		    case SDLK_F2:
-		    case SDLK_F3:
-		    case SDLK_F4:
-		    case SDLK_F5:
-		    case SDLK_F6:
-		    case SDLK_F7:
-		    case SDLK_F8:
-		    case SDLK_F9:
-		    case SDLK_F10:
-		    case SDLK_F11:
-		    case SDLK_F12:
-		    case SDLK_F13:
-		    case SDLK_F14:
-		    case SDLK_F15:
-			if (IHandler.FunctionKeyStatus)
-			    action |= INP_KEYBOARD + INP_FUNCTION_KEY;
-			break;
+                        case SDLK_F1:
+                        case SDLK_F2:
+                        case SDLK_F3:
+                        case SDLK_F4:
+                        case SDLK_F5:
+                        case SDLK_F6:
+                        case SDLK_F7:
+                        case SDLK_F8:
+                        case SDLK_F9:
+                        case SDLK_F10:
+                        case SDLK_F11:
+                        case SDLK_F12:
+                        case SDLK_F13:
+                        case SDLK_F14:
+                        case SDLK_F15:
+                            if (IHandler.FunctionKeyStatus)
+                                action |= INP_KEYBOARD + INP_FUNCTION_KEY;
+                            break;
 
-		    default:
-			break;
-		    }
-		}
-		break;
+                        default:
+                            break;
+                    }
+                    break;
 
-	    case SDL_MOUSEMOTION:
-		if (IHandler.MouseExists && IHandler.MouseStatus) {
-		    if ((l_Mask & INP_LEFT) && (ev.motion.xrel < 0))
-			action |= INP_MOUSE + INP_LEFT;
-		    if ((l_Mask & INP_RIGHT) && (ev.motion.xrel > 0))
-			action |= INP_MOUSE + INP_RIGHT;
-		    if ((l_Mask & INP_UP) && (ev.motion.yrel < 0))
-			action |= INP_MOUSE + INP_UP;
-		    if ((l_Mask & INP_DOWN) && (ev.motion.yrel > 0))
-			action |= INP_MOUSE + INP_DOWN;
-		}
-		break;
+                case SDL_MOUSEMOTION:
+                    if (IHandler.MouseExists && IHandler.MouseStatus) {
+                        if ((l_Mask & INP_LEFT) && (ev.motion.xrel < 0))
+                            action |= INP_MOUSE + INP_LEFT;
+                        if ((l_Mask & INP_RIGHT) && (ev.motion.xrel > 0))
+                            action |= INP_MOUSE + INP_RIGHT;
+                        if ((l_Mask & INP_UP) && (ev.motion.yrel < 0))
+                            action |= INP_MOUSE + INP_UP;
+                        if ((l_Mask & INP_DOWN) && (ev.motion.yrel > 0))
+                            action |= INP_MOUSE + INP_DOWN;
+                    }
+                    break;
 
-	    case SDL_MOUSEBUTTONDOWN:
-		if (IHandler.MouseExists && IHandler.MouseStatus) {
-		    if ((l_Mask & INP_LBUTTONP) && ev.button.button == SDL_BUTTON_LEFT) {
-			action |= INP_MOUSE + INP_LBUTTONP;
-		    }
-		    if ((l_Mask & INP_RBUTTONP) && ev.button.button == SDL_BUTTON_RIGHT) {
-			action |= INP_MOUSE + INP_RBUTTONP;
-		    }
-		}
-		break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (IHandler.MouseExists && IHandler.MouseStatus) {
+                        if ((l_Mask & INP_LBUTTONP)
+                            && ev.button.button == SDL_BUTTON_LEFT) {
+                            action |= INP_MOUSE + INP_LBUTTONP;
+                        }
+                        if ((l_Mask & INP_RBUTTONP)
+                            && ev.button.button == SDL_BUTTON_RIGHT) {
+                            action |= INP_MOUSE + INP_RBUTTONP;
+                        }
+                    }
+                    break;
 
-    /*
-	    case SDL_JOYBUTTONDOWN:
-		if (IHandler.JoyExists) {
-		    switch (ev.jbutton.button) {
-		    case 0:
-			action |= INP_MOUSE + INP_LBUTTONP;
-			break;
+                    /*
+                       case SDL_JOYBUTTONDOWN:
+                       if (IHandler.JoyExists) {
+                       switch (ev.jbutton.button) {
+                       case 0:
+                       action |= INP_MOUSE + INP_LBUTTONP;
+                       break;
 
-		    case 1:
-			action |= INP_MOUSE + INP_RBUTTONP;
-			break;
+                       case 1:
+                       action |= INP_MOUSE + INP_RBUTTONP;
+                       break;
 
-		    default:
-			break;
-		    }
-		}
-		break;
-    */
-	    default:
-		break;
-	    }
-	}
+                       default:
+                       break;
+                       }
+                       }
+                       break;
+                     */
+                default:
+                    break;
+            }
+        }
 
         inpClearKbBuffer();
-	inpDoPseudoMultiTasking();
+        inpDoPseudoMultiTasking();
     }
 
     return action;
@@ -292,8 +291,8 @@ void inpDelay(S32 l_Ticks)
     S32 i;
 
     for (i = 0; i < l_Ticks; i++) {
-	gfxWaitTOF();
-	inpDoPseudoMultiTasking();
+        gfxWaitTOF();
+        inpDoPseudoMultiTasking();
     }
 }
 
@@ -306,14 +305,14 @@ void inpSetKeyRepeat(unsigned char rate)
     interval = (rate & 0x1f);
 
     if (delay == 0)
-	delay = 1000;
+        delay = 1000;
     else
-	delay = 750;
+        delay = 750;
 
     if (interval == 0)
-	interval = 30;
+        interval = 30;
     else
-	interval = 60;
+        interval = 60;
 
 /*    SDL_EnableKeyRepeat(delay, interval);*/
 
