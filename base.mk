3c4c0107bc7fef5c224aafc36deecf9bc3ada2e5
Vladimir Vrzić <vvrzic@gmail.com>
2024-08-26 15:48:28 +0200

    Updated paths in makefiles

--- base.mk
+++ base.mk
@@ -1,26 +1,24 @@
-WARN	= -pedantic -Wall -Wmissing-prototypes -Wmissing-declarations -Wsign-compare
-OPT	= -O2 -funroll-loops
-CFLAGS	= -I. -std=c99 $(OPT) $(WARN) -g $$($(SDL2_CONFIG) --cflags)
-LDFLAGS = $$($(SDL2_CONFIG) --libs) -lm
-
-SRCDIR  = .
-BINARY  = $(BINDIR)/derclou
-
-RM ?= rm
-
+SRCDIR  = src
 SOURCES != find $(SRCDIR) -name '*.c'
 DIRS    != find $(SRCDIR) -mindepth 1 -maxdepth 1 -type d | cut -d/ -f2
-OBJECTS := $(addprefix $(OBJDIR)/,$(SOURCES:%.c=%.o))
+OBJECTS := $(addprefix $(OBJDIR)/,$(SOURCES:$(SRCDIR)/%.c=%.o))
+BINARY  = $(BINDIR)/derclou
+
+WARN	= -pedantic -Wall -Wmissing-prototypes -Wmissing-declarations -Wsign-compare
+OPT	= -O2 -funroll-loops
+CFLAGS	= -I$(SRCDIR) -std=c99 $(OPT) $(WARN) -g $$($(SDL2_CONFIG) --cflags)
+LDFLAGS = $$($(SDL2_CONFIG) --libs) -lm
+
+RM ?= rm -f
 
 .PHONY: all clean
 
 all: $(BINARY)
 
-
 $(BINARY): $(BINDIR) $(OBJECTS)
 	$(CC) -o $(BINARY) $(OBJECTS) $(LDFLAGS)
 
-$(OBJDIR)/%.o: %.c
+$(OBJDIR)/%.o: $(SRCDIR)/%.c
 	$(CC) $(CFLAGS) -I$(HEADERDIR) -I$(dir $<) -c $< -o $@
 
 $(BINDIR):
